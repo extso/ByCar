b69ab749c2672e714a7f98a5956b7769cc975c46
extso <extso@outlook.com>
2014-01-12 21:30:50 +0530

    more HTML changes
    -- Robin

---
+++ scripts/custom/controller.js
@@ -0,0 +1,74 @@
+var controller = function(){
+	var self =  this;
+	self.updateCityListView();
+	self.activateEvents();
+	return self;
+};
+
+controller.prototype.activateEvents = function(){
+	var self = this;
+	$('ul.cityListItem > li > a').on("click", self.changeView);
+};
+
+controller.prototype.changeView = function(event){
+	var self = this;
+	var selectedCity = event.currentTarget.text;
+	// $('.cityListItem').on("click", function(){
+		$('#homeContainer').hide();
+		$('#citySelection').show();
+		$('body').css('min-height', '2000px');
+		$('body').css('padding-top', '70px');
+		$('.container').css('max-width', '1170px');
+		$('.cityNameDisplay > a').text(selectedCity);
+		$('#cityPostCount').text("007");// to change the text of post count badge
+	// });
+};
+
+controller.prototype.fetchCityList = function(){
+	var self = this;
+	var cityData = {
+	cityList : {Chandigarh_Tricity : [
+									"Chandigarh",
+									"Mohali",
+									"Panchkula"]
+				,NCR : [
+									"Noida",
+									"Delhi",
+									"Gurgaon",
+									"Ghaziabad",
+									"Faridabad"]
+				,Bangalore: {}
+				,Pune: {}
+				,Mumbai: {}
+				,Chennai: {}
+				,Hyderabad: {}
+				,Ahemdabad: {}
+				,Jaipur: {}
+				,Kochi: {}
+				,Bhubneshwar: {}
+				,Kolkata: {}
+				,Others: []
+				}
+	};
+
+	delete cityData.cityList["Others"];
+	var cityList = Object.keys(cityData.cityList).sort();
+	cityList.push("Others");
+	return cityList;
+};
+
+controller.prototype.updateCityListView = function(){
+	var self = this;
+	var cityList = self.fetchCityList();
+	_.each(cityList, function(city){
+		if(city !== "Others"){
+			var listItem = '<li><a href="#">'+ city +'</a></li>';
+		}
+		else {
+			var listItem = '<li class="divider"></li><li><a href="#">'+ city +'</a></li>';
+		}
+		$('ul.cityListItem').append(listItem);
+	});
+};
+
+new controller();
